4ad8867b52d01f65b40ccb91052179d01903f634
yango3 <o3.yang@hotmail.com>
2023-04-12 14:47:34 +0800

    tutorial 1 OK

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ## GStreamer
 
+output -> Makefile compile output for *.o , main
+
+build -> for CMake compile directory
+
 ### macos
 Manual compile.
 
 ```
 clang -c main.c -o main.o `pkg-config --cflags gstreamer-1.0`
 clang -o main main.o `pkg-config --libs gstreamer-1.0`
+
+clang basic1.c -o basic1 `pkg-config --cflags --libs gstreamer-1.0`
 
 ```
 
---
+++ src/basic1.c
@@ -0,0 +1,51 @@
+#include <gst/gst.h>
+
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#endif
+
+int tutorial_main(int argc, char *argv[], void *args){
+    GstElement *pipeline;
+    GstBus     *bus;
+    GstMessage *msg;
+
+    // initialize GStreamer
+    gst_init(&argc, &argv);
+
+    // build the pipeline
+    pipeline = gst_parse_launch(
+        "playbin url=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm",
+        NULL
+    );
+
+    // start playing
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    // wait until error or EOS */
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(
+            bus,
+            GST_CLOCK_TIME_NONE,
+            GST_MESSAGE_ERROR| GST_MESSAGE_EOS
+    );
+
+    if(GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR){
+        g_error("An error occured!");
+    }
+
+    // free resources
+    gst_message_unref(msg);
+    gst_object_unref(bus);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+    
+    return 0;
+}
+
+int main(int argc, char*argv[]){
+#if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
+    return gst_macos_main(tutorial_main, argc, argv, NULL);
+#else
+    return tutorial_main(argc, argv);
+#endif
+}
