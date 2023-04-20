1e027308cfe9e66b2a61483c974de9c0bbe8c910
yango3 <o3.yang@hotmail.com>
2023-04-20 16:16:41 +0800

    add airplae params

--- gst-app/src/airplane.c
+++ gst-app/src/airplane.c
@@ -1,12 +1,24 @@
 #include "gst-app.h"
 
+typedef struct _Params{
+    gchar host[128];
+    guint port;
+    gchar video[24];
+} Params;
+
+Params params = {
+    .host="127.0.0.1",
+    .port=1234,
+    .video="h.264"
+};
+
 int main(int argc, char *argv[]){
 
     g_print("In airplane app ...\n");
 
     // commandline options parsing
 
-    
+
 
     return 0;
 }
