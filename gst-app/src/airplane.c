6cb6d1c47ba3e2f44045367c4536449abbe9bd48
yango3 <o3.yang@hotmail.com>
2023-04-20 16:48:03 +0800

    add airplane goption

--- gst-app/src/airplane.c
+++ gst-app/src/airplane.c
@@ -4,20 +4,52 @@ typedef struct _Params{
     gchar host[128];
     guint port;
     gchar video[24];
+    gboolean version;
 } Params;
 
 Params params = {
     .host="127.0.0.1",
     .port=1234,
-    .video="h.264"
+    .video="h.264",
+    .version=FALSE
 };
+gchar version[] = "version: 0.1";
 
+int parse_params(int argc, char**argv){
+    const GOptionEntry entries[] = {
+        {"version", 'v', 0, G_OPTION_ARG_NONE,&params.version,"executable version", NULL },
+        { NULL,}
+    };
+    GOptionContext *ctx;
+    GError *err = NULL;
+    gint i, num;
+
+    ctx = g_option_context_new("airplane params");
+    g_option_context_add_group(ctx, gst_init_get_option_group());
+    g_option_context_add_main_entries(ctx, entries, NULL);
+
+    if(!g_option_context_parse(ctx, &argc, &argv, &err)){
+        g_print("Error command line: %s\n", GST_STR_NULL(err->message));
+        return -1;
+    }
+
+    g_option_context_free(ctx);
+
+    return 0;
+}
 int main(int argc, char *argv[]){
 
     g_print("In airplane app ...\n");
 
     // commandline options parsing
+    if(parse_params(argc, argv) != 0){
+        return -1;
+    }
 
+    if (params.version == TRUE){
+        g_print("%s\n", version);
+        return 0;
+    }
 
 
     return 0;
