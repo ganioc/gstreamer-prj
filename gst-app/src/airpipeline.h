7c0f874718e8fba9ec90407ca5c2ee9772fc5683
yango3 <o3.yang@hotmail.com>
2023-04-21 10:33:13 +0800

    add airpipeline.c

---
+++ gst-app/src/airpipeline.h
@@ -0,0 +1,9 @@
+#ifndef _MY_APP_AIR_PIPELINE_INCLUDED_
+#define _MY_APP_AIR_PIPELINE_INCLUDED_
+
+#include <gst/gst.h>
+
+int demo(void);
+int run_pipeline(int argc, char *argv[], void *args);
+
+#endif
