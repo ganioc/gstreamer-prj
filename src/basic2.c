586f8db9b78f5121d463d68983d3e57ac9c127d0
yango <o3.yang@hotmail.com>
2023-04-22 15:26:38 +0800

    Add __linux__ os type

--- src/basic2.c
+++ src/basic2.c
@@ -4,9 +4,10 @@
 #include <TargetConditionals.h>
 #endif
 
-int tutorial_main(int argc, char *argv[], void* args){
-    GstElement *pipeline, *source , *sink;
-    GstBus     *bus;
+int tutorial_main(int argc, char *argv[], void *args)
+{
+    GstElement *pipeline, *source, *sink;
+    GstBus *bus;
     GstMessage *msg;
     GstStateChangeReturn ret;
 
@@ -17,31 +18,34 @@ int tutorial_main(int argc, char *argv[], void* args){
     source = gst_element_factory_make("videotestsrc", "source");
     sink = gst_element_factory_make("autovideosink", "sink");
 
-    // create the empty pipeline 
+    // create the empty pipeline
     pipeline = gst_pipeline_new("test-pipeline");
 
-    if(!pipeline || !source || !sink){
+    if (!pipeline || !source || !sink)
+    {
         g_printerr("Not all elements can be created\n");
         return -1;
     }
 
     // build the pipeline,
     gst_bin_add_many(GST_BIN(pipeline),
-        source,
-        sink,
-        NULL);
-    if(gst_element_link(source, sink) != TRUE){
+                     source,
+                     sink,
+                     NULL);
+    if (gst_element_link(source, sink) != TRUE)
+    {
         g_printerr("Elements can not be linked\n");
         gst_object_unref(pipeline);
         return -1;
     }
 
     // modify the source's properties
-    g_object_set(source, "pattern", 0, NULL);
+    g_object_set(source, "pattern", 1, NULL);
 
     // start playing,
     ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    if( ret == GST_STATE_CHANGE_FAILURE){
+    if (ret == GST_STATE_CHANGE_FAILURE)
+    {
         g_printerr("Unable to set the pipeline to the playing state\n");
         gst_object_unref(pipeline);
         return -1;
@@ -50,11 +54,12 @@ int tutorial_main(int argc, char *argv[], void* args){
     // wait until error or EOS
     bus = gst_element_get_bus(pipeline);
     msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-        GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+                                     GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
     // parse message
-    if(msg != NULL){
-        GError * err;
+    if (msg != NULL)
+    {
+        GError *err;
         gchar *debug_info;
 
         switch (GST_MESSAGE_TYPE(msg))
@@ -63,19 +68,19 @@ int tutorial_main(int argc, char *argv[], void* args){
             /* code */
             gst_message_parse_error(msg, &err, &debug_info);
             g_printerr("Error received from element: %s: %s\n",
-                GST_OBJECT_NAME(msg->src),
-                err->message);
+                       GST_OBJECT_NAME(msg->src),
+                       err->message);
             g_printerr("Debugging information : %s\n",
-                debug_info? debug_info: "none");
+                       debug_info ? debug_info : "none");
             g_clear_error(&err);
             g_free(debug_info);
             break;
         case GST_MESSAGE_EOS:
             g_print("Enod of Stream reached\n");
             break;
-        
+
         default:
-        // should not reach here,
+            // should not reach here,
             g_printerr("Unexpected message received.\n");
             break;
         }
@@ -89,11 +94,13 @@ int tutorial_main(int argc, char *argv[], void* args){
     return 0;
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 #if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
-  return gst_macos_main (tutorial_main, argc, argv, NULL);
+    return gst_macos_main(tutorial_main, argc, argv, NULL);
+#elif defined(__linux__)
+    return tutorial_main(argc, argv, NULL);
 #else
-  return tutorial_main (argc, argv, NULL);
+    g_print("Unknown OS type\n");
 #endif
 }
